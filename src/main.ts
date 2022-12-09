2147c83cb0291011ac4a7579d88fd7eb15714f8b
Joshua Harwood <jharwood@harwoodspike.com>
2022-12-08 21:30:33 -0700

    Update aws-exports.js path

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 // @ts-ignore
-import config from './aws-exports.js';
+import config from '@/aws-exports.js';
 import { Amplify } from "aws-amplify";
 
 const pinia = createPinia()
