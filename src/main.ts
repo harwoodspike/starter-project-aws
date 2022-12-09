6c762ae6bc3d36bd18dbb5b9eaaa3753ad0f74e6
Joshua Harwood <jharwood@harwoodspike.com>
2022-12-09 07:22:47 -0700

    Remove ts ignore

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-// @ts-ignore
 import config from '@/aws-exports.js';
 import { Amplify } from "aws-amplify";
 
